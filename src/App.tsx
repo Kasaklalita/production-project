3f1f043ba993d2b5c5e001eb04e75796b8f442c7
Veniamin Polienko <kasaklalita@mail.ru>
2023-06-12 11:49:03 +0300

    Роутинг Code Splitting Lasy Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from "./components/Counter";
+import { Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <div className="app">
-      asdf
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
