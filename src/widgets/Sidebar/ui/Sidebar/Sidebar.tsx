2608eb6de07f9583d3ba4bbb7c4b13c4019c0a24
Veniamin Polienko <kasaklalita@mail.ru>
2023-06-12 17:22:20 +0300

    Added Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { useState } from "react";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsedd] = useState(false);
+
+  const onToggle = () => {
+    setCollapsedd((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={onToggle}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher className={"asdf"} />
+        {/* langSwitcher */}
+      </div>
+    </div>
+  );
+};
